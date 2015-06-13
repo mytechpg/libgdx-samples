cd0ae98940be483d7d4f5f5e0a1a377e0cdcfab3
mytechpg <mytechpg@gmail.com>
2015-06-13 23:07:19 +0200

    added Logging example without any change/add

---
+++ logging/desktop/src/it/blogspot/mytechpg/tests/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package it.blogspot.mytechpg.tests.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import it.blogspot.mytechpg.tests.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
---
+++ logging/html/src/it/blogspot/mytechpg/tests/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package it.blogspot.mytechpg.tests.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import it.blogspot.mytechpg.tests.MyGdxGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new MyGdxGame();
+        }
+}
