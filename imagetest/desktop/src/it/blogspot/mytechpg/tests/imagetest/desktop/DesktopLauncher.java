0e14c03fab29b84b82469a42b1d5c31702cd515c
mytechpg <mytechpg@gmail.com>
2015-07-15 00:36:41 +0200

    added old branch

---
+++ imagetest/desktop/src/it/blogspot/mytechpg/tests/imagetest/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package it.blogspot.mytechpg.tests.imagetest.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import it.blogspot.mytechpg.tests.imagetest.ImageTest;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ImageTest(), config);
+	}
+}
